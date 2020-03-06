5cea65e7aae3447ac61aae6f9ed2e5413ec86cc4
Maarten van der Sande <maartenvandersande@hotmail.com>
2020-03-06 13:44:27 +0100

    forgot to add rest

--- test/test_04_dataloader_integration.py
+++ test/test_04_dataloader_integration.py
@@ -1,5 +1,4 @@
 import unittest
-from torch.utils.data import DataLoader
 
 import peaksql
 from test.test_02_database import DATABASE_BED
@@ -13,10 +12,3 @@ class TestMLIntegration(unittest.TestCase):
         for seq, label in dataset:
             assert seq.shape == (3, 4,)
             assert label.shape == (1,)
-
-    def test_402_Integration_PyTorch_DataLoader(self):
-        dataset = peaksql.BedRegionDataSet(DATABASE_BED, nr_rand_pos=100, seq_length=3)
-        dataloader = DataLoader(dataset, batch_size=10)
-        for seq, label in dataloader:
-            assert tuple(seq.shape) == (10, 3, 4,)
-            assert tuple(label.shape) == (10, 1,)
